154f0cdfd33a8ea3ddc8beb65ee3059a61b504b6
arunkudiyal <arunkudiyal@icloud.com>
2023-06-22 10:10:56 +0530

    Searching in BST

--- src/com/company/BinarySearchTree.java
+++ src/com/company/BinarySearchTree.java
@@ -29,12 +29,25 @@ public class BinarySearchTree {
         postOrderTraversal(root);
         System.out.println();
     }
+    public boolean search(int key) {
+        Node address = searchKey(root, key);
+        if(address == null) return false;
+        else return true;
+    }
     private Node insertData(Node root, int data) {
         // 1. Tree is not available
         if(root == null) root = new Node(data);
         // 2. Tree is available
         else if(root.data > data) root.left = insertData(root.left, data);
         else if(root.data < data) root.right = insertData(root.right, data);
+        return root;
+    }
+    // NOTE -> searchKey returns an address of the node when the data is found
+    // & returns null if the data is not found
+    private Node searchKey(Node root, int key) {
+        if(root == null || key == root.data) return root;
+        else if(key < root.data) return searchKey(root.left, key);
+        else if(key > root.data) return searchKey(root.right, key);
         return root;
     }
     private void inOrderTraversal(Node root) {
@@ -70,10 +83,17 @@ public class BinarySearchTree {
         bst.insert(17);
         bst.insert(92);
         // NOTE :- inOrder of any BST is always in the ascending order.
-        bst.inOrder();                      // 4 9 17 20 23 30 32 77 92
+        bst.inOrder();                                                          // 4 9 17 20 23 30 32 77 92
         // NOTE :- First output of preOrder prints the root
-        bst.preOrder();                     // 30 20 9 4 17 23 32 77 92
-        // NOTE :- Last output of preOrder prints the root
-        bst.postOrder();                    // 4 17 9 23 20 92 77 32 30
+        bst.preOrder();                                                         // 30 20 9 4 17 23 32 77 92
+        // NOTE :- Last output of postOrder prints the root
+        bst.postOrder();                                                        // 4 17 9 23 20 92 77 32 30
+        System.out.println(bst.search(77));                                // true
+        System.out.println(bst.search(100));                               // false
+
+        // QUESTIONS BASED ON ORDER OF BST :-
+        // Q.1 - Given a preOrder of a BST, find the postOrder & inOrder for the BST. (POSSIBLE)
+        // Q.2 - Given a postOrder of a BST, find the preOrder & inOrder for the BST. (POSSIBLE)
+        // Q.3 - Given a inOrder of a BST, find the preOrder & postOrder for the BST. (NOT POSSIBLE)
     }
 }
