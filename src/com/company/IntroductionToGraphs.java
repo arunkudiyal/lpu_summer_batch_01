34f942a8c232b94c5f68366ca6bf5e798c58354a
arunkudiyal <arunkudiyal@icloud.com>
2023-06-27 11:43:19 +0530

    Basics of Graph - getEdges() & containsSelfLoop()

--- src/com/company/IntroductionToGraphs.java
+++ src/com/company/IntroductionToGraphs.java
@@ -10,6 +10,7 @@ public class IntroductionToGraphs {
         this.V = vertices;
         this.graph = new int[V][V];
     }
+    // ASSIGNMENT :- You can describe any graph using Adjacency Matrix, is there any other possibility to represent a graph?
     public void display() {
         for(int i=0; i < V; i++) {
             for(int j=0; j < V; j++) {
@@ -18,24 +19,43 @@ public class IntroductionToGraphs {
             System.out.println();
         }
     }
+    public boolean containsSelfLoop() {
+        for(int i=0; i < V; i++) {
+            for(int j=0; j < V; j++) {
+                if(i == j) if(graph[i][j] == 1) return true;
+            }
+        }
+        return false;
+    }
+    public int getEdges() {
+        int count = 0;
+        for(int i=0; i < V; i++) {
+            for(int j=0; j < V; j++){
+                if(graph[i][j] == 1) count+=1;
+            }
+        }
+        return count/2;
+    }
     public static void main(String[] args) {
-        /*
-                            6
-                            0 1 0 1 0 1
-                            1 0 1 1 1 0
-                            0 1 0 1 1 1
-                            1 1 1 0 1 0
-                            0 1 1 1 0 1
-                            1 0 1 0 1 0
-         */
+/*
+6
+0 1 0 1 0 1
+1 0 1 1 1 0
+0 1 0 1 1 1
+1 1 1 0 1 0
+0 1 1 1 0 1
+1 0 1 0 1 0
+*/
         Scanner sc = new Scanner(System.in);
         int V = sc.nextInt();
-        IntroductionToGraphs g = new IntroductionToGraphs(V);                       // V = 6, graph[6][6];
-        for(int i=0; i < g.V; i++) {                                                // i = 0 1 2 3 4 5
-            for(int j=0; j < g.V; j++) {                                            // j = 0 1 2 3 4 5
+        IntroductionToGraphs g = new IntroductionToGraphs(V);                           // V = 6, graph[6][6];
+        for(int i=0; i < g.V; i++) {                                                    // i = 0 1 2 3 4 5
+            for(int j=0; j < g.V; j++) {                                                // j = 0 1 2 3 4 5
                 g.graph[i][j] = sc.nextInt();
             }
         }
         g.display();
+        System.out.println("Total number of edges in the graph - " + g.getEdges());
+        System.out.println("Graph contains self loop - " + g.containsSelfLoop());       // false
     }
 }
