3464bf408e1a351b8339cf077fab8d82a57a5aa9
arunkudiyal <arunkudiyal@icloud.com>
2023-07-01 10:51:04 +0530

    Prims Algorithm for finding the sum of MST

--- src/com/company/PrimsAlgorithmMST.java
+++ src/com/company/PrimsAlgorithmMST.java
@@ -20,22 +20,49 @@ public class PrimsAlgorithmMST {
         }
         // Step3 -> Make the entry point of the graph
         cost[0] = graph[0][0];
-        for(int i=0; i < V; i++) {
+        // for in total of V-1 edges
+        for(int i=0; i < V-1; i++) {
             // minCostVertex is a function which gives the value of the vertex
             // which has not been visited but has the least cost
             int u = minCostVertex(cost, visited);
             visited[u] = true;
             // I am looking for all the neighbors of u
             for(int v=0; v < V; v++) {
-                // if(there is a edge AND not visited AND the cost is min
-                if(graph[u][v] != 0 && !visited[v] && graph[u][v] < cost[v]) {
+                // if(there is an edge AND not visited AND the cost is min
+                if(graph[u][v] != 0 && !visited[v] && graph[u][v] <= cost[v]) {
                     cost[v] = graph[u][v];
                 }
             }
         }
         // Create a display which is going to build the MST
-
+        int totalCost = 0;
+        for(int i=0; i < V; i++)
+            totalCost += cost[i];
+        System.out.println(totalCost);
     }
+    public int minCostVertex(int[] cost, boolean[] visited) {
+        int min = Integer.MAX_VALUE; int min_vertex = -1;
+        for(int i=0; i < V; i++) {
+            if(cost[i] <= min && !visited[i]) {
+                min = cost[i];
+                min_vertex = i;
+            }
+        }
+        return  min_vertex;
+    }
+/*
+9
+0 4 0 0 0 0 0 8 0
+4 0 8 0 0 0 0 11 0
+0 8 0 7 0 4 0 0 2
+0 0 7 0 9 14 0 0 0
+0 0 0 9 0 10 0 0 0
+0 0 4 14 10 0 2 0 0
+0 0 0 0 0 2 0 1 6
+8 11 0 0 0 0 1 0 7
+0 0 2 0 0 0 6 7 0
+37
+*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int V = sc.nextInt();
