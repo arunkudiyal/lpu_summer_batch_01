875a8a40ffadef2f58e33ca62d2cb312a7980f88
arunkudiyal <arunkudiyal@icloud.com>
2023-07-05 09:15:20 +0530

    Knapsack Problem - Complete

--- src/com/company/KnapsackProblem.java
+++ src/com/company/KnapsackProblem.java
@@ -3,21 +3,23 @@ package com.company;
 import java.util.Scanner;
 
 public class KnapsackProblem {
-    public int knapSackUtil(int W, int[] weight, int[] price, int n, int[][] table) {
-        if(n == 0 || W == 0) return 0;
-        if(table[n][W] != -1) return table[n][W];
-        if(weight[n-1] > W) return table[n][W] = knapSackUtil(W, weight, price, n-1, table);
-        else return
+    public int findMax(int a, int b) {
+        return Math.max(a, b);
     }
-    public int knapSack(int W, int[] weight, int[] profit, int n) {
+    public int sackUtil(int W, int[] weight, int[] price, int n, int[][] table) {
+        if(n == 0 || W == 0) return 0;
+        else if(table[n][W] != -1) return table[n][W];
+        else if(weight[n-1] > W) return sackUtil(W, weight, price, n-1, table);
+        else return table[n][W] = findMax(sackUtil(W, weight, price, n-1, table), price[n-1] + sackUtil(W-weight[n-1], weight, price, n-1, table));
+    }
+    public int sack(int W, int[] weight, int[] price, int n) {
         int[][] table = new int[n+1][W+1];
         for(int i=0; i <= n; i++) {
             for(int j=0; j <= W; j++) {
-                // -1 represents that the value is not found.
                 table[i][j] = -1;
             }
         }
-        return knapSackUtil(W, weight, profit, n, table);
+        return sackUtil(W, weight, price, n, table);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -35,6 +37,6 @@ public class KnapsackProblem {
         }
         int W = sc.nextInt();
         KnapsackProblem kSack = new KnapsackProblem();
-        System.out.println(kSack.knapSack(W, weight, profit, n));;
+        System.out.println(kSack.sack(W, weight, profit, n));
     }
 }
